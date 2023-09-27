ea2d7bfc7c79721e9a718a05d32d3e27032fb55c
Korbin Heffron <korbinheffron06@gmail.com>
2023-09-27 13:33:08 -0500

    decryption using a wordlist; filters bad results

---
+++ dictionary_attack.py
@@ -0,0 +1,20 @@
+
+ciphertext = input("type your code to decrypt: ")
+ciphertext_bytes = bytes.fromhex(ciphertext)
+
+
+with open("wordlist.txt", "r") as file:
+
+    for line in file:
+        key = line.strip().encode()
+        decrypted = bytearray()
+
+        for i in range(len(ciphertext_bytes)):
+            decrypted.append(ciphertext_bytes[i] ^ key[i % len(key)])
+
+        if all(32 <= byte <= 126 for byte in decrypted):
+            print("trying: " + line)
+            print("decrypted (ASCII): ", decrypted.decode())
+        
+
+    
